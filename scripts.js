d7cb9b2467ede0f7901389adacb62e04fb58df11
NariaNexis <nnanimefreak5@gmail.com>
2023-01-16 13:14:12 -0800

    added index title and another welcome message

--- scripts.js
+++ scripts.js
@@ -1,5 +1,5 @@
 window.onload = function() {
-    const title = ["Hello Jon", "Funny Seeing You Here &#128065 "]
+    const title = ["Hello Jon", "Funny Seeing You Here &#128065", "Worm Zone", ]
     const random = Math.floor(Math.random() * title.length);
     console.log (random)
    document.querySelector("h1").innerHTML = title[random]
